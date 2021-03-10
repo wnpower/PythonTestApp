5b02703ff8fae40df814c0517501e56cbd0af9f6
Ignacio Morandin <35805127+imorandinwnp@users.noreply.github.com>
2021-03-10 12:10:45 -0300

    Update passenger_wsgi.py

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,1 +1,6 @@
+import sys, os
+
+INTERP = os.path.dirname(os.path.abspath(__file__)) + "/venv/bin/python"
+if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
+  
 from miapp import app as application
