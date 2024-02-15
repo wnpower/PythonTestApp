32b6663246b9ec411dc9e4c738aad333230de70b
Ignacio Morandin <imorandin@wnpower.com>
2024-02-15 11:44:02 -0300

    update app

--- miapp.py
+++ miapp.py
@@ -1,11 +1,11 @@
 from flask import Flask
 import os
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route("/")
+@application.route("/")
 def hello():
     return "This is Hello World!\n"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=12345)
+    application.run(host='0.0.0.0', port=12345)
--- passenger_wsgi.py
+++
@@ -1,6 +0,0 @@
-import sys, os
-
-INTERP = os.path.dirname(os.path.abspath(__file__)) + "/venv/bin/python"
-if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
-  
-from miapp import app as application
