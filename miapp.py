966ff6cd2e3fbf2b0fdb14e2f8810aa4d7baa15b
Ignacio Morandin <imorandin@wnpower.com>
2024-04-10 11:03:36 -0300

    add version

--- miapp.py
+++ miapp.py
@@ -1,11 +1,12 @@
 from flask import Flask
 import os
+import sys
 
 MyApp = Flask(__name__)
 
 @MyApp.route("/")
 def hello():
-    return "This is Hello World!\n"
+    return "This is Hello World!. Python version: " + sys.version + "\n"
 
 if __name__ == "__main__":
     MyApp.run(host='0.0.0.0', port=12345)
